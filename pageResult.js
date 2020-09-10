240a0a680b347b2d31161d3bc41c31d700dd14a9
gitName <a@b.c>
2020-09-10 11:15:18 +0200

    Cleanup unused code from lighthouse report fetcher

--- pageResult.js
+++ pageResult.js
@@ -1,4 +1,3 @@
-// const fs = require('fs');
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 
@@ -9,14 +8,6 @@ module.exports = async function (url) {
     const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless', '--no-sandbox'] });
     const options = { logLevel: 'error', output: 'json', port: chrome.port };
     const runnerResult = await lighthouse(url, options);
-
-    // `.report` is the HTML report as a string
-    // const reportHtml = runnerResult.report;
-    // fs.writeFileSync('lhreport.html', reportHtml);
-
-    // `.lhr` is the Lighthouse Result as a JS object
-    // console.log('Report is done for', runnerResult.lhr.finalUrl);
-    // console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
 
     const endTime = Date.now();
     console.log(`Request executed in ${endTime - startTime} milliseconds`);
