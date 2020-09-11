bbf30a3a9fd0f9b61355e71f394e578750e261d0
gitName <a@b.c>
2020-09-11 11:31:57 +0200

    Just run performance and seo tests, skip some audits to save time

--- pageResult.js
+++ pageResult.js
@@ -1,6 +1,73 @@
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 
+const seoSkipAudits = ['structured-data', 'robots-txt'];
+const performanceSkipAudits = [
+    'first-cpu-idle',
+    'screenshot-thumbnails', 
+    'final-screenshot',
+    'max-potential-fid',
+    'first-meaningful-paint',
+    'estimated-input-latency',
+    'render-blocking-resources',
+    'uses-responsive-images',
+    'offscreen-images',
+    'unminified-css',
+    'unminified-javascript',
+    'unused-css-rules',
+    'unused-javascript',
+    'uses-optimized-images',
+    'uses-webp-images',
+    'uses-text-compression',
+    'uses-rel-preconnect',
+    'server-response-time',
+    'redirects',
+    'uses-rel-preload',
+    'uses-http2',
+    'efficient-animated-content',
+    'duplicated-javascript',
+    'legacy-javascript',
+    'total-byte-weight',
+    'uses-long-cache-ttl',
+    'dom-size',
+    'critical-request-chains',
+    'user-timings',
+    'bootup-time',
+    'mainthread-work-breakdown',
+    'font-display',
+    'performance-budget',
+    'timing-budget',
+    'resource-summary',
+    'third-party-summary',
+    'largest-contentful-paint-element',
+    'layout-shift-elements',
+    'uses-passive-event-listeners',
+    'no-document-write',
+    'long-tasks',
+    'non-composited-animations',
+    'large-javascript-libraries',
+    'network-requests',
+    'network-rtt',
+    'network-server-latency',
+    'main-thread-tasks',
+    'diagnostics',
+    'metrics'
+];
+
+/* These are the audits I want performed
+but others get included, so that's why 
+the skipAudits is used to make sure we
+exclude the unwanted audits and results
+*/
+// const performanceOnlyAudits = [
+//     'first-contentful-paint',
+//     'speed-index',
+//     'largest-contentful-paint',
+//     'interactive',
+//     'total-blocking-time',
+//     'cumulative-layout-shift'
+// ];
+
 module.exports = async function (url) {
     const startTime = Date.now();
     console.log(url);
@@ -21,7 +88,7 @@ module.exports = async function (url) {
         '--disable-gpu-early-init',
         '--disable-gpu-memory-buffer-compositor-resources',
         '--disable-gl-drawing-for-tests',
-        '--disable-gpu',        
+        '--disable-gpu',
         '--use-gl=desktop', // better cpu usage with --use-gl=desktop rather than --use-gl=swiftshader, still needs more testing.
         '--enable-webgl',
         '--hide-scrollbars',
@@ -30,8 +97,9 @@ module.exports = async function (url) {
         '--ignore-gpu-blacklist',
         '--no-sandbox',
     ];
+    
     const chrome = await chromeLauncher.launch({ chromeFlags: args });
-    const options = { logLevel: 'error', output: 'json', port: chrome.port };
+    const options = { logLevel: 'error', output: 'json', port: chrome.port, onlyCategories: ['seo', 'performance'], skipAudits: [...performanceSkipAudits, ...seoSkipAudits] };
     const runnerResult = await lighthouse(url, options);
 
     const endTime = Date.now();
@@ -39,8 +107,8 @@ module.exports = async function (url) {
     await chrome.kill();
 
     return {
-        "accessibility": runnerResult.lhr.categories["accessibility"],
-        "best-practices": runnerResult.lhr.categories["best-practices"],
+        "accessibility": null,
+        "best-practices": null,
         "performance": runnerResult.lhr.categories["performance"],
         "seo": runnerResult.lhr.categories["seo"],
     }
